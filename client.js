696ffc1574173618326bcf8e94d3c3f9502931e4
Sura Jeon <surajeon@gmail.com>
2021-05-05 21:24:55 +0000

    now my mini snake can move

---
+++ client.js
@@ -0,0 +1,36 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: SJ');
+
+    let num = 15;
+    holdTime = 50;
+    for (let i = 0; i < num; i ++) {
+      setTimeout(() => {
+        setTimeout(() => {
+          conn.write('Move: left');          
+        }, holdTime);
+        conn.write('Move: down');
+      }, holdTime);
+      holdTime += 50;
+    }
+
+  });
+
+  conn.on("data", (message) => {
+    console.log(message);
+  });
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
-  conn.on("data", () => {
-    console.log("Good bye!");
-  });
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting...");
 
