636db999276ed331240fb8c1aafbe1752d23927e
Sura Jeon <surajeon@gmail.com>
2021-05-05 22:09:55 +0000

    created a function to respond to a particular key

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
+// const { stdin } = require('node:process');
 const connect = require('./client');
 
 console.log("Connecting...");
 
-connect();
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// connect();
+setupInput();
